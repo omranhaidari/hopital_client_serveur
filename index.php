6d7b0f67e59c2703ba9667be7336991b68f218f8
Alexis Colin <alexis.colin5@orange.fr>
2019-11-12 11:56:16 +0100

    Ajout "Hello World"

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
+<?php 
+header( 'content-type: text/html; charset=utf-8' );
 
-<?php include("header.php");
+//include("header.php");
+//include("rechercheMultiCrieteres.php");
+//include("affichageDetails.php");
+//include("affichageResultat.php"); 
+//include("footer.php"); 
 
-include("rechercheMultiCrieteres.php");
+echo'
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Hopital</title>
+	</head>
+	
+	<body>
+		<h1>Hello World</h1>
+	</body>
+</html>';
 
-include("affichageDetails.php");
-
-include("affichageResultat.php"); 
-
-include("footer.php"); ?>
+?>
