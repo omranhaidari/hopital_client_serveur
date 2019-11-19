5221f1afb8cffaad372950f8ae5205f557bcbadd
omranhaidari <omran.haidari@etu.univ-lyon1.fr>
2019-11-19 23:26:04 +0100

    Revert "ajout message aucun résultat trouvé"
    This reverts commit 4e2f2475967394f9b44ae6462fe532229e4bf083.

--- resultat_recherche.php
+++ resultat_recherche.php
@@ -6,24 +6,14 @@
 		<h2>Résultats de la recherche</h2>
 
 			<ul>';
-		
-     
-         if(mysqli_num_rows($resultat)==0){        
-             echo 'Aucun resulat trouve';
-         }else{
-	
-                while($dataR3 = mysqli_fetch_array($resultat))
-                        
-      
-                               
+				// Pour chaque résultat récupéré par la requête, il y a l'affichage des noms et prénoms des patients correspondants à la recherche, sous forme de liste et de lien hypertexte (avec le code/identifiant du patient en paramètre, par l'URL).
+				while($dataR3 = mysqli_fetch_array($resultat))
+				{
 					echo'<li><a href="index.php?param='.utf8_encode($dataR3["code"]).'">'.utf8_encode($dataR3["nom"]).' '.utf8_encode($dataR3["prenom"]).'</a></li>';
-        }
-        
-         }
-                
+				}
 			echo'
 			</ul>
 		</div>';
-	
+	}
 
 ?>
