e7c62f5f41d7068d286e4677eb8d4894030dd78e
Alexis Colin <alexis.colin5@gmail.com>
2019-11-19 11:30:37 +0100

    Changement nom fichier CSS

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	echo'
 	<head>
 		<meta charset="utf-8" />
-		<link href="css/index.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
 		<title>Hôpital Didier LEFEBVRE</title>
 		<h1>Hôpital Didier LEFEBVRE</h1>
 	<head>';
